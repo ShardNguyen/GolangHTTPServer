accfa98f20e6c81c9bb40ad99e3c10d3f41df885
ShardNguyen <shardnguyen@gmail.com>
2025-03-18 14:11:13 +0700

    add setters and getters to user struct

--- pkg/entity/user.go
+++ pkg/entity/user.go
@@ -1,6 +1,40 @@
 package entity
 
+import "errors"
+
 type User struct {
-	Id   int    `json:"id"`
-	Name string `json:"name"`
+	id   int    `json:"id"`
+	name string `json:"name"`
+}
+
+func (u *User) GetID() (int, error) {
+	if u == nil {
+		return 0, errors.New("User is not initialized")
+	}
+
+	return u.id, nil
+}
+
+func (u *User) GetName() (string, error) {
+	if u == nil {
+		return "", errors.New("User is not initialized")
+	}
+
+	return u.name, nil
+}
+
+func (u *User) SetID(id int) error {
+	if u == nil {
+		return errors.New("User is not initialized")
+	}
+	u.id = id
+	return nil
+}
+
+func (u *User) SetName(name string) error {
+	if u == nil {
+		return errors.New("User is not initialized")
+	}
+	u.name = name
+	return nil
 }
